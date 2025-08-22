487d4cdf381ebe5ee8c50bc8b9be141b42339430
jp-lima <jpbrasil763@gmail.com>
2025-08-22 15:35:57 -0300

    Função Pesquisa

--- src/routes/ListaPacientes.jsx
+++ src/routes/ListaPacientes.jsx
@@ -11,9 +11,45 @@ const ListaPacientes = () => {
 
   const [pacientesEncontrados, setPacientesEncontrados] = useState(pacientesMock);
 
+
+  const Nn = (valor) => {
+    console.log(!isNaN(valor))
+  }
+
   
+    const Pesquisar = ( valor ) => {
+
+      valor = valor.toLowerCase()
+      let lista = []
+
+      if(valor.length === 0){
+      setPacientesEncontrados(pacientesMock)  
+      }else{
+        setPacientesEncontrados([])
+      }
+      
+      for(let i =0; pacientesMock.length > i;i++ ){
+        
+        let paciente = pacientesMock[i]
+        
+        let numero = paciente.celular
+        let cpf = paciente.cpf
+        let nome = paciente.nome.toLowerCase()
+
+        // Se for número, retorna true
+        if(numero.includes(valor) || cpf.includes(valor) || nome.includes(valor)){
+          console.log(paciente)
+          lista = [...lista,paciente]
+
+          setPacientesEncontrados(lista)
+ 
+        }
+
+      
     
-  console.log(pacientesMock);
+    
+    }
+    }
 
   return (
   <div className='page_lista-pacientes'>
